9355cf7adc01f456a162ccc8cf7e2e955014aab0
ivan <oraionstarr@gmail.com>
2026-04-20 19:02:08 +0800

    feat: added smooth scroll using lenis

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -1,8 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { useEffect } from "react";
+import Lenis from "lenis";
+import gsap from "gsap";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    });
+
+    (window as any).__lenis = lenis;
+
+    const tick = (time: number) => {
+      lenis.raf(time * 1000);
+    };
+
+    gsap.ticker.add(tick);
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      gsap.ticker.remove(tick);
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
       {children}
