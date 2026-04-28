d9487849cea26fab93066692d30d5f9bf3a3e4d8
ivan <oraionstarr@gmail.com>
2026-04-28 22:52:29 +0800

    feat: adding all sections to the main file

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+import Navbar from "./components/ui/Navbar";
 import Home from "./components/sections/Home";
+import About from "./components/sections/About";
+import Works from "./components/sections/Works";
+import Portfolio from "./components/sections/Portfolio";
+import Skills from "./components/sections/Skills";
+import Certificates from "./components/sections/Certificates";
+import Footer from "./components/ui/Footer";
 
-export default function Portfolio() {
+export default function Root() {
   return (
     <>
-      <Home />
+      <Navbar />
+      <main>
+        <Home />
+        <About />
+        <Works />
+        <Portfolio />
+        <Skills />
+        <Certificates />
+      </main>
+      <Footer />
     </>
   );
 }
